c8fb64e7c154c9437bedbe770d6cb54a255d1f0c
Roland Hartanto <13515107@std.stei.itb.ac.id>
2017-11-04 16:55:26 +0700

    add LED status controller with switch

--- main/main.ino
+++ main/main.ino
@@ -1,12 +1,50 @@
 //Mobile, Inc
 //Group: Chlordane
 
-void setup() {
-  // put your setup code here, to run once:
+/*********** GLOBAL VARIABLES ***********/
+//Pin number constant
+const int ledPin = 13; //LED indicator
+const int switchPin = 12; //Tactile switch
 
+//Indicator LED on/off status
+int indicatorLEDState = LOW;
+
+//tactile switch previous status
+int prevStatus = LOW;
+
+//time
+long prevMilis = 0;
+long debounce = 200;
+
+/*********** CONTROLLER ***********/
+//LED Status Controller
+void indicatorLEDSwitch();
+
+/*********** PIN DECLARATION ***********/
+void setup() {
+  pinMode(ledPin, OUTPUT); 
+  pinMode(switchPin, INPUT_PULLUP); 
 }
 
+/*********** MAIN PROGRAM ***********/
 void loop() {
   // put your main code here, to run repeatedly:
+  indicatorLEDSwitch();
+}
 
+//
+void indicatorLEDSwitch(){
+  int switchStatus;
+  
+  switchStatus = digitalRead(switchPin);
+  if(switchStatus == HIGH && prevStatus == LOW && millis() - prevMilis > debounce) {
+    if(indicatorLEDState == LOW){
+      indicatorLEDState = HIGH;
+    }else{
+      indicatorLEDState = LOW;
+    }  
+    prevMilis = millis();
+  }
+  digitalWrite(ledPin,indicatorLEDState); 
+  prevStatus = switchStatus;
 }
